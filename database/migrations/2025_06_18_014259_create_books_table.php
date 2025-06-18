9db1fe66669b694629c5f0dc1adc8a4addd3ffcf
Wellington <wellington.bonfim@hotmail.com>
2025-06-18 00:12:28 -0300

    feat: adicionar migration para criar tabela books

---
+++ database/migrations/2025_06_18_014259_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', length: 50);
+            $table->string('publisher', 50);
+            $table->integer('edition');
+            $table->string('published_year', 4);
+            $table->decimal('value', 8, 2); // valor do livro
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
