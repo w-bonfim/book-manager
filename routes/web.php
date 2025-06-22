f5ab7590ab0d6059dff25247372453e58d81dbb9
Wellington <wellington.bonfim@hotmail.com>
2025-06-21 22:42:24 -0300

    feat: rotas definidas para Livro, Autor e Assunto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\SubjectController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('subjects', SubjectController::class);
