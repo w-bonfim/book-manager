2624d8f42aec31c0826b37332debce83ec158f61
Wellington <wellington.bonfim@hotmail.com>
2025-06-22 01:03:40 -0300

    feat: adicionado seeder para criação de usuário de autenticação

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@bookmanager.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('123'),
+            ]
+        );
+    }
+}
