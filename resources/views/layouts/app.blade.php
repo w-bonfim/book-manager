55b9ee8242ead4b26676fc1f9bcd9ea95b2f6816
Wellington <wellington.bonfim@hotmail.com>
2025-06-21 22:32:18 -0300

    feat: implementado layout com Bootstrap na última versão

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Book Manager')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    @yield('styles')
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="#">Book Manager</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('books.index') }}">Livros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('authors.index') }}">Autores</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('subjects.index') }}">Assuntos</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container">
+        @yield('content')
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    @yield('scripts')
+</body>
+</html>
